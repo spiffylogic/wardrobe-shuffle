8e69fc73cd9a41e7b13be12b7e31240cb66b4dc8
Mark <spiffylogic@gmail.com>
2017-10-29 19:11:32 -0400

    Added a click listeners to the buttons

--- app/src/main/java/me/spiffylogic/wardrobeshuffle/MainActivity.kt
+++ app/src/main/java/me/spiffylogic/wardrobeshuffle/MainActivity.kt
@@ -2,11 +2,29 @@ package me.spiffylogic.wardrobeshuffle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.support.design.widget.Snackbar
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manageButton = findViewById<Button>(R.id.manage_button)
+        manageButton.setOnClickListener {
+            Snackbar.make(it, "Go to manage wardrobe (inventory)", Snackbar.LENGTH_LONG).show()
+        }
+
+        val shuffleButton = findViewById<Button>(R.id.shuffle_button)
+        shuffleButton.setOnClickListener({ view ->
+            Snackbar.make(view, "Tell me what to wear", Snackbar.LENGTH_LONG).show()
+        })
     }
+
+
+
+
 }
